d15fca9a916c2e14c4ffb278da3643aa288d70dd
FedericoCarboni <fr3ddydev@gmail.com>
2020-11-12 16:26:11 +0100

    feat: add outputs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import * as path from 'path';
+import * as os from 'os';
+
 import * as core from '@actions/core';
 import { install } from './install';
 
+const EXE_EXT = os.platform() === 'win32' ? '.exe' : '';
+
 const main = async () => {
   try {
-    await install();
+    const installPath = await install();
+    core.addPath(installPath);
+    core.setOutput('path', installPath);
+    core.setOutput('ffmpeg-path', path.join(installPath, `ffmpeg${EXE_EXT}`));
+    core.setOutput('ffprobe-path', path.join(installPath, `ffprobe${EXE_EXT}`));
   } catch (error) {
     core.setFailed(error);
   }
