8c20b3c2c3e825c74829e76e49800562b3b0dd92
FedericoCarboni <fr3ddydev@gmail.com>
2020-11-13 08:56:25 +0100

    fix: extraction on windows

--- src/install.ts
+++ src/install.ts
@@ -7,6 +7,8 @@ import * as core from '@actions/core';
 import * as hc from '@actions/http-client';
 import * as tc from '@actions/tool-cache';
 
+const firstChild = (dir: string) => fs.readdirSync(dir)[0];
+
 const linux = async () => {
   const fetchVersion = async (retry = 10): Promise<string> => {
     try {
@@ -32,7 +34,7 @@ const linux = async () => {
   const downloadPath = await tc.downloadTool('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz');
   core.info(`Extracting ffmpeg from ${downloadPath}`);
   const extractPath = await tc.extractTar(downloadPath, void 0, ['-x']);
-  const sourceDir = path.join(extractPath, fs.readdirSync(extractPath)[0]);
+  const sourceDir = path.join(extractPath, firstChild(extractPath));
   core.info(`Caching ffmpeg from ${sourceDir}`);
   const cachedPath = await tc.cacheDir(sourceDir, 'ffmpeg', version);
   core.info(`Cached ffmpeg to ${cachedPath}`);
@@ -64,7 +66,7 @@ const windows = async () => {
   const downloadPath = await tc.downloadTool('https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z');
   core.info(`Extracting ffmpeg from ${downloadPath}`);
   const extractPath = await tc.extract7z(downloadPath);
-  const sourceDir = path.join(extractPath, 'bin');
+  const sourceDir = path.join(extractPath, firstChild(extractPath), 'bin');
   core.info(`Caching ffmpeg from ${sourceDir}`);
   const cachedPath = await tc.cacheDir(sourceDir, 'ffmpeg', version);
   core.info(`Cached ffmpeg to ${cachedPath}`);
