2e562c187e0c1fae10f453db59d0fbeae1f96eed
FedericoCarboni <fr3ddydev@gmail.com>
2020-11-13 15:01:14 +0100

    refactor: move fetchVersion

--- src/install.ts
+++ src/install.ts
@@ -6,31 +6,34 @@ import * as os from 'os';
 import * as core from '@actions/core';
 import * as hc from '@actions/http-client';
 import * as tc from '@actions/tool-cache';
+import * as rh from '@actions/tool-cache/lib/retry-helper';
 
+const userAgent = 'FedericoCarboni/setup-ffmpeg';
 const firstChild = (dir: string) => fs.readdirSync(dir)[0];
 
+const fetch = async (url: string): Promise<string> => {
+  const retryHelper = new rh.RetryHelper(3, 10, 20);
+  return retryHelper.execute(async () => {
+    const http = new hc.HttpClient(userAgent, [], {
+      allowRetries: false,
+    });
+    const response = await http.get(url);
+    if (response.message.statusCode !== 200)
+      throw new tc.HTTPError(response.message.statusCode);
+    return await response.readBody();
+  }, (err) => {
+    core.info(err.message);
+    if (err instanceof tc.HTTPError)
+      return true;
+    return false;
+  });
+};
+
 const linux = async () => {
-  const fetchVersion = async (retry = 40): Promise<string> => {
-    try {
-      const client = new hc.HttpClient('FedericoCarboni/setup-ffmpeg', [], {
-        socketTimeout: 1000,
-      });
-      const response = await client.get('https://johnvansickle.com/ffmpeg/release-readme.txt');
-      const readme = await response.readBody();
-      const [, version] = /version: (.*?)\n/.exec(readme) ?? [];
-      assert.ok(version);
-      return version;
-    } finally {
-      core.info('Failed to fetch latest version...');
-      if (retry) {
-        core.info('Retrying in 1 second...');
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        return await fetchVersion(retry - 1);
-      }
-    }
-  };
   core.info('Fetching version...');
-  const version = await fetchVersion();
+  const info = await fetch('https://johnvansickle.com/ffmpeg/release-readme.txt');
+  const [, version] = /version: (.*?)\n/.exec(info) ?? [];
+  assert.ok(version);
   core.info(`Downloading ffmpeg v${version}`);
   const downloadPath = await tc.downloadTool('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz');
   core.info(`Extracting ffmpeg from ${downloadPath}`);
@@ -43,26 +46,9 @@ const linux = async () => {
 };
 
 const windows = async () => {
-  const fetchVersion = async (retry = 40): Promise<string> => {
-    try {
-      const client = new hc.HttpClient('FedericoCarboni/setup-ffmpeg', [], {
-        socketTimeout: 1000,
-      });
-      const response = await client.get('https://www.gyan.dev/ffmpeg/builds/release-version');
-      const body = await response.readBody();
-      const [version] = body.trim().split('-');
-      assert.ok(version);
-      return version;
-    } finally {
-      core.info('Failed to fetch latest version...');
-      if (retry) {
-        core.info('Retrying...');
-        return await fetchVersion(retry - 1);
-      }
-    }
-  };
   core.info('Fetching version...');
-  const version = await fetchVersion();
+  const info = await fetch('https://www.gyan.dev/ffmpeg/builds/release-version');
+  const [version] = info.trim().split('-');
   core.info(`Downloading ffmpeg v${version}`);
   const downloadPath = await tc.downloadTool('https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z');
   core.info(`Extracting ffmpeg from ${downloadPath}`);
