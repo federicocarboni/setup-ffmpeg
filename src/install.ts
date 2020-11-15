0e0c93803436542267e655f774568e2984ddfc08
FedericoCarboni <fr3ddydev@gmail.com>
2020-11-15 19:26:20 +0100

    refactor: rewrite

--- src/install.ts
+++ src/install.ts
@@ -3,49 +3,55 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as os from 'os';
 
-import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as hc from '@actions/http-client';
 import * as tc from '@actions/tool-cache';
 
 const userAgent = 'FedericoCarboni/setup-ffmpeg';
 
+const ext = os.platform() === 'win32' ? '.exe' : '';
+
+const getExePath = (dir: string, filename: string) => path.join(dir, filename + ext);
+const chmodX = (filename: string) => fs.promises.chmod(filename, '755');
+
 const fetchVersion = async (): Promise<string> => {
   const http = new hc.HttpClient(userAgent);
-  const release = (await http.getJson<{ tag_name: string; }>(
-    'https://api.github.com/repos/FedericoCarboni/setup-ffmpeg/releases/latest')).result;
+  const response = await http.getJson<any>('https://api.github.com/repos/FedericoCarboni/setup-ffmpeg/releases/latest');
+  assert.ok(response.statusCode === 200);
+  const release = response.result;
   const version = release?.tag_name;
   assert.ok(version);
   return version;
 };
 
-const testInstallation = async (installPath: string) => {
-  core.info('testing installation');
-  assert.ok(await exec.exec(path.join(installPath, `ffmpeg${EXE_EXT}`), ['-version']) === 0, 'Expected ffmpeg to exit with code 0');
-  assert.ok(await exec.exec(path.join(installPath, `ffprobe${EXE_EXT}`), ['-version']) === 0, 'Expected ffprobe to exit with code 0');
-  core.info('installation successful');
+const testInstallation = async (ffmpegPath: string, ffprobePath: string) => {
+  assert.ok(await exec.exec(ffmpegPath, ['-version']) === 0, 'Expected ffmpeg to exit with code 0');
+  assert.ok(await exec.exec(ffprobePath, ['-version']) === 0, 'Expected ffprobe to exit with code 0');
 };
 
-export const EXE_EXT = os.platform() === 'win32' ? '.exe' : '';
-
-export const install = async (): Promise<string> => {
-  // TODO: support 32-bit
+export const install = async (): Promise<{
+  path: string;
+  ffmpegPath: string;
+  ffprobePath: string;
+}> => {
   assert.strictEqual(os.arch(), 'x64');
-
-  let installPath = tc.find('ffmpeg', '4.x');
-  if (installPath) {
-    core.info(`found cached installation ${installPath}`);
-    return installPath;
-  }
+  assert.ok(os.platform() === 'linux' || os.platform() === 'win32');
 
   const version = await fetchVersion();
-  const downloadPath = await tc.downloadTool(`https://github.com/FedericoCarboni/setup-ffmpeg/releases/download/4.3.1/ffmpeg-${os.platform()}-${os.arch()}.tar.gz`);
-  const extractPath = await tc.extractTar(downloadPath);
-  installPath = await tc.cacheDir(extractPath, 'ffmpeg', version, os.arch());
 
-  await fs.promises.chmod(path.join(installPath, `ffmpeg${EXE_EXT}`), '755');
-  await fs.promises.chmod(path.join(installPath, `ffprobe${EXE_EXT}`), '755');
+  let path = tc.find('ffmpeg', version);
+  if (!path) {
+    const downloadPath = await tc.downloadTool(`https://github.com/FedericoCarboni/setup-ffmpeg/releases/download/4.3.1/ffmpeg-${os.platform()}-${os.arch()}.tar.gz`);
+    const extractPath = await tc.extractTar(downloadPath);
+    path = await tc.cacheDir(extractPath, 'ffmpeg', version, os.arch());
+  }
 
-  await testInstallation(installPath);
-  return installPath;
+  const ffmpegPath = getExePath(path, 'ffmpeg');
+  const ffprobePath = getExePath(path, 'ffprobe');
+
+  await chmodX(ffmpegPath);
+  await chmodX(ffprobePath);
+
+  await testInstallation(ffmpegPath, ffprobePath);
+  return { path, ffmpegPath, ffprobePath };
 };
