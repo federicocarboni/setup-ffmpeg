e2720c491aa40abbc3214026d1013fc617100dd2
FedericoCarboni <fr3ddydev@gmail.com>
2021-02-08 21:23:27 +0100

    fix: allow github tokens to avoid rate limits

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,18 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as os from 'os';
 
+import * as gh from '@octokit/rest';
 import * as core from '@actions/core';
-import * as hc from '@actions/http-client';
 import * as tc from '@actions/tool-cache';
 import * as exec from '@actions/exec';
+import { createActionAuth } from '@octokit/auth-action';
 
-const GITHUB_REPO = 'FedericoCarboni/setup-ffmpeg';
-const GITHUB_URL = `https://github.com/${GITHUB_REPO}`;
+const owner = 'FedericoCarboni';
+const repo = 'setup-ffmpeg';
+const GITHUB_URL = `https://github.com/${owner}/${repo}`;
 const PLATFORMS = new Set(['linux', 'win32', 'darwin']);
 
-interface Release {
-  tag_name: string;
-}
-
-// sets the file as executable acts like chmod +x $path
+// Sets the file as executable acts like chmod +x $path
 const chmodx = (path: string) => fs.promises.chmod(path, '755');
 
 async function main() {
@@ -24,18 +22,20 @@ async function main() {
     const platform = os.platform();
     const arch = os.arch();
 
-    // check if the current platform and architecture are supported
+    // Check if the current platform and architecture are supported
     assert.ok(PLATFORMS.has(platform), `setup-ffmpeg cannot be run on ${platform}`);
-    assert.ok(arch === 'x64', 'setup-ffmpeg can only be run on 64-bit systems');
+    assert.strictEqual(arch, 'x64', 'setup-ffmpeg can only be run on 64-bit systems');
 
-    // fetch the latest build of ffmpeg
-    const http = new hc.HttpClient('FedericoCarboni/setup-ffmpeg');
-    const res = await http.getJson<Release[]>(`https://api.github.com/repos/${GITHUB_REPO}/releases`);
+    // Fetch the latest build of ffmpeg
+    const auth = await createActionAuth()().catch(() => void 0);
 
-    assert.ok(res.statusCode === 200);
-    assert.ok(res.result);
+    const octokit = new gh.Octokit({ auth });
+    const releases = await octokit.repos.listReleases({ owner, repo });
 
-    const tagName = res.result.find(({ tag_name }) => tag_name.startsWith('ffmpeg-'))?.tag_name;
+    assert.ok(releases.status === 200);
+    assert.ok(releases.data);
+
+    const tagName = releases.data.find(({ tag_name }) => tag_name.startsWith('ffmpeg-'))?.tag_name;
 
     assert.ok(tagName);
 
@@ -43,13 +43,13 @@ async function main() {
 
     assert.ok(version);
 
-    // search in the cache if version is already installed
+    // Search in the cache if version is already installed
     let installPath = tc.find('ffmpeg', version, arch);
 
-    // if ffmpeg was not found in cache download it from releases
+    // If ffmpeg was not found in cache download it from releases
     if (!installPath) {
       const downloadURL = `${GITHUB_URL}/releases/download/${tagName}/ffmpeg-${platform}-${arch}.tar.gz`;
-      const downloadPath = await tc.downloadTool(downloadURL);
+      const downloadPath = await tc.downloadTool(downloadURL, void 0, auth?.token);
       const extractPath = await tc.extractTar(downloadPath);
       installPath = await tc.cacheDir(extractPath, 'ffmpeg', version, arch);
     }
@@ -60,11 +60,11 @@ async function main() {
     const ffmpegPath = path.join(installPath, `ffmpeg${ext}`);
     const ffprobePath = path.join(installPath, `ffprobe${ext}`);
 
-    // ensure the correct permission to execute ffmpeg and ffprobe
+    // Ensure the correct permission to execute ffmpeg and ffprobe
     await chmodx(ffmpegPath);
     await chmodx(ffprobePath);
 
-    // execute ffmpeg -version and ffprobe -version to verify the installation
+    // Execute ffmpeg -version and ffprobe -version to verify the installation
     assert.ok(await exec.exec(ffmpegPath, ['-version']) === 0);
     assert.ok(await exec.exec(ffprobePath, ['-version']) === 0);
 
