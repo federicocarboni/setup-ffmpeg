e6790939a1de478ceb29759f20998ce36d857beb
Federico Carboni <48333714+FedericoCarboni@users.noreply.github.com>
2023-12-19 19:40:33 +0100

    don't output commonjs to webpack

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
-};
+module.exports = (api) => ({
+  presets: [['@babel/preset-env', {
+    targets: {
+      node: 'current',
+      esmodules: !api.env('NODE_TEST'),
+    },
+  }]],
+});
