b8c859f7ff8053580d0665f5cb77d3a45e085e8d
thesn10 <38666407+thesn10@users.noreply.github.com>
2024-02-02 22:00:18 +0100

    run build

--- dist/index.js
+++ dist/index.js
@@ -60113,11 +60113,11 @@ class GyanInstaller {
    * @param release {import('./installer').ReleaseInfo}
    * @returns {Promise<import('./installer').InstalledTool>}
    */
-  async downloadTool(release) {
+  async downloadTool(release, linkingType) {
     const downloadPath = await tool_cache.downloadTool(release.downloadUrl[0]);
     const extractPath = await tool_cache.extract7z(downloadPath, null, _7ZR_PATH);
     const dir = external_path_.join(extractPath, (await (0,promises_namespaceObject.readdir)(extractPath))[0], 'bin');
-    const toolInstallDir = await tool_cache.cacheDir(dir, this.toolCacheDir, release.version, 'x64');
+    const toolInstallDir = await tool_cache.cacheDir(dir, this.toolCacheDir, release.version + "-" + linkingType, 'x64');
     return {
       version: release.version,
       path: toolInstallDir,
@@ -60408,7 +60408,7 @@ class EvermeetCxInstaller {
  * @property {boolean} [skipIntegrityCheck]
  * @property {string} toolCacheDir
  * @property {string} [githubToken]
- * @property {string} [linkingType]
+ * @property {string} linkingType
  */
 
 /**
@@ -60484,7 +60484,7 @@ async function install(options) {
   if (!release) release = await getRelease(installer, options);
   core.info(`Installing ffmpeg version ${release.version} from ${release.downloadUrl}`);
   return {
-    ...(await installer.downloadTool(release)),
+    ...(await installer.downloadTool(release, options.linkingType)),
     cacheHit: false,
   };
 }
